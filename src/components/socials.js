dd43efdeab2c6daa3355428b468ab850fc72d30a
Ralph Andy Menz <andyralphmenz@gmail.com>
2024-12-06 08:37:56 +0000

    ...

--- src/components/socials.js
+++ src/components/socials.js
@@ -5,20 +5,20 @@ import Twitter from "../components/images/Bird Icon 376969.svg";
 import LinkedIn from "../components/images/linkedin.svg";
 import GitHub from "../components/images/GitHub.svg";
 
-import FaceBookLink from "../components/images/Facebook Icon.svg";
-import InstagramLink from "../components/images/Instagram Icon.svg";
-import TwitterLink from "../components/images/Bird Icon 376969.svg";
-import LinkedInLink from "../components/images/linkedin.svg";
-import GitHubLink from "../components/images/GitHub.svg";
+// import FaceBookLink from "../components/images/Facebook Icon.svg";
+// import InstagramLink from "https://www.instagram.com/menz_andy/";
+// import TwitterLink from "https://x.com/AndyRalphMenz";
+// import LinkedInLink from "https://www.linkedin.com/in/andy-ralph-menz-32bb10259/";
+// import GitHubLink from "https://github.com/NyxRalph";
 
 export default function Socials() {
   return (
     <div className="socials__logo">
-      <img className="Twitter Logo_social" src={Twitter} alt="Twitter Logo" onClick={TwitterLink} />
-      <img className="Facebook Logo_social"src={FaceBook} alt="FaceBook Logo" onClick={FaceBookLink}  />
-      <img className="Instagram Logo_social"src={Instagram} alt="Instagram Logo" onClick={InstagramLink}  />
-      <img className="LinkedIn Logo_social"src={LinkedIn} alt="LinkedIn Logo" onClick={LinkedInLink}  />
-      <img className="GitHub Logo_social"src={GitHub} alt="GitHub Logo" onClick={GitHubLink}  />
+      <img className="Twitter Logo_social" src={Twitter} alt="Twitter Logo"  />
+      <img className="Facebook Logo_social"src={FaceBook} alt="FaceBook Logo"   />
+      <img className="Instagram Logo_social"src={Instagram} alt="Instagram Logo"  />
+      <img className="LinkedIn Logo_social"src={LinkedIn} alt="LinkedIn Logo"   />
+      <img className="GitHub Logo_social"src={GitHub} alt="GitHub Logo"  />
     </div>
   );
 }
